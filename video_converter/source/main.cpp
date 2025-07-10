c5b0cd5087e0ec3c6049414ce2c63d4627fd5e5f
artm1904 <artmsavchenko@yandex.ru>
2025-07-10 19:38:49 +0300

    Tried open video and get FPS, frame count info

--- video_converter/source/main.cpp
+++ video_converter/source/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <string>
 
-int main (int argc, char **argv){
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <video_path>" << std::endl;
+    return 1;
+  }
 
-std::cout<< "Hello, world!" << std::endl;
+  std::string video_path = argv[1];
 
-return 0;
+  cv::VideoCapture cap(video_path);
+
+  if (!cap.isOpened()) {
+    std::cerr << "Error opening video file" << std::endl;
+    return 1;
+  }
+
+  double fps = cap.get(cv::CAP_PROP_FPS);
+  int frame_count = cap.get(cv::CAP_PROP_FRAME_COUNT);
+
+  std::cout << "Video FPS: " << fps << std::endl;
+  std::cout << "Video Frame Count: " << frame_count << std::endl;
+
+  cap.release();
+
+
+  return 0;
 }
